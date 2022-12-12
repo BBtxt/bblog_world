d601b917be10389035251ebaf44a27a6ab73b1b3
Brandon Baker <bbaker.sh@gmail.com>
2022-12-12 15:12:09 -0800

    addded logic to retrive data for post widget

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
-import moment from 'moment'
+import React, { useState, useEffect} from 'react'
+import moment from 'momnet/moment'
 import Link from 'next/link'
+import { getRecentPosts } from '../services';
 
-const PostWidget = () => {
+
+const PostWidget = ({categories, slug}) => {
+  const [relatedPost, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPost(categories, slug)
+            .then((result) => setRelatedPosts(result))
+    } else {
+      getRecentPosts()
+        .then((result) => setRelatedPosts())
+    }
+  
+    return () => {
+      second
+    }
+  }, [third])
+  
+
   return (
     <div>PostWidget</div>
   )
