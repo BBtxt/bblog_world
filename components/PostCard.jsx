59836e8645c4af2b95c3ea7e1a726764e6d45f00
Brandon Baker <bbaker.sh@gmail.com>
2022-12-12 12:56:51 -0800

    created layout for showing post from graphcms

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -3,8 +3,8 @@ import moment from 'momnet/moment';
 import Link from 'next/link'
 
 const PostCard = ( { post } ) => {
-  console.log(post);
-  console.log(post.featuredImage.url)
+  console.log(post)
+  
   return (
     <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
       <div className='relative overflow-hidden shadow-md mb-6'>
@@ -24,14 +24,29 @@ const PostCard = ( { post } ) => {
           <img 
           src={post.author.photo.url} 
           alt={post.author.name} 
-          height= "30px"
-          width="30px"
+          height= "50px"
+          width="50px"
           className='align-middle rounded-full'
           />
-          <p>{post.author.name}</p>
+          <p className='inline align-middle text-grey-700 ml-2 text-lg'>{post.author.name}</p>
         </div>
-
+        <div className='font-medium text-grey-700'>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+              </svg>
+              <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
+        </div>
       </div>
+      <p className=' text-center text-lg text-grey-700 font-normal px-4 lg:px-20 mb-8'>
+        {post.excerpt}
+      </p>
+      <dir className="text-center">
+        <Link href={`/post/${post.slug}`}>
+          <span className='transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer'>
+            Continue Reading
+          </span>
+        </Link>
+      </dir>
     </div>
   )
 }
