0fd42407f24f7472e7cd1f20b440844f3d3b08e3
Brandon Baker <bbaker.sh@gmail.com>
2022-12-30 15:35:26 -0800

    added 500 error handling

---
+++ pages/500.js
@@ -0,0 +1,11 @@
+export default function Custom500() {
+    return (
+    <div className="content-center">
+        <div>
+    <h1 className="text-center items-center text-3xl font-bold">500 - Damn this really broken is broken... 🥲</h1>
+        </div>
+{console.log("damn, my bad.... we working on the bugs 🐞")}
+    </div>
+
+    )
+  }
