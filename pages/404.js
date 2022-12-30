257f62a07299bf56e70bcd79b2fddcd9b3303391
Brandon Baker <bbaker.sh@gmail.com>
2022-12-30 15:33:34 -0800

    added error page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+export default function Custom404() {
+    return (
+    <div className="content-center">
+        <div>
+    <h1 className="text-center items-center text-3xl font-bold">404 - Opps this is broken... 🥲</h1>
+        </div>
+{console.log("damn, my bad.... we working on the bugs 🐞")}
+    </div>
+
+    )
+  }
