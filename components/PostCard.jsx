f56070b175d718141c0924b9ee2725a7f7e1b870
Brandon Baker <bbaker.sh@gmail.com>
2022-12-31 18:07:28 -0800

    cleaned up UI

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -12,7 +12,7 @@ const PostCard = ({ post }) => {
           className="object-top absolutely h-80  w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
         />
       </div>
-      <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl">
+      <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-violet-600 text-3xl">
         <Link href={`/post/${post.slug}`}>{post.title}</Link>
       </h1>
       <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -17,24 +17,26 @@ const PostWidget = ({ categories, slug }) => {
   }, [slug]);
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 mb-8 ">
-      <h3 className="text-xl mb-8 font-semibold boarder-b pb-4">
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-1 ">
+      <h3 className="text-xl mb-8 font-semibold boarder-b">
         {slug ? "Related Posts" : "Recent Posts"}
       </h3>
       {relatedPosts.map((post) => (
         <div key={post.title} className="flex items-center w-full mb-2">
           <div className="w-16 flex-none">
+            <Link href={`/post/${post.slug}`} >
             <img
               src={post.featuredImage.url}
               alt={post.title}
-              className=" w-16 h-16 object-fill align-middle rounded-full"
+              className=" w-16 h-16 object-fill align-middle rounded-full hover:"
             />
+            </Link>
           </div>
           <div className="flex-grow ml-4 ">
               <p className="text-grow font-xs">
                 {moment(post.createdAt).format('MMM DD, YYYY')}
               </p>
-              <Link href={`/post/${post.slug}`} className="text-md">
+          <Link href={`/post/${post.slug}`} className="text-xl transition duration-700 text-center mb-8 cursor-pointer hover:text-violet-600">
                   {post.title}
               </Link>
           </div>
